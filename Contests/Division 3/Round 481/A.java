cb0159c34a6121751ba6155fbfb11be3c4d0f99b
jsn665 <64424116+jsn665@users.noreply.github.com>
2021-06-26 21:22:48 -0400

    Add files via upload

---
+++ Contests/Division 3/Round 481/A.java
@@ -0,0 +1,212 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class A
+{
+	static int MOD = 1000000007;
+    static Scanner in = new Scanner(System.in);
+    static PrintWriter p = new PrintWriter(System.out, true);
+
+
+
+    static int solve(){
+		int n = in.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++){
+			arr[i] = in.nextInt();
+		}
+		
+		Set<Integer> set = new HashSet<>();
+		List<Integer> list = new ArrayList<>();
+		for (int i = n - 1; i >= 0; i--){
+			int curr = arr[i];
+			if (!set.contains(curr)){
+				list.add(curr);
+			}
+			set.add(curr);
+		}
+		p.println(list.size());
+		for (int i = list.size() - 1; i >= 0; i--){
+			p.print(list.get(i) + " ");
+		}
+		
+		p.close();
+        return 0;
+    }
+
+    public static void main(String[] args)
+    {
+        solve();
+    }
+	
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+ 
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { return Integer.parseInt(next()); }
+ 
+        long nextLong() { return Long.parseLong(next()); }
+ 
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static class F{
+
+
+         static void swap(int[] a, int i, int j){
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+
+        static void swap(String[] a, int i, int j){
+            String temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+
+        static void swap(long[] a, int i, int j){
+            long temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+
+        static void swap(double[] a, int i, int j){
+            double temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+		
+		static int[] readArrI(int N){
+			int[] res = new int[N];
+			for (int i = 0; i < N; i++){
+				res[i] = in.nextInt();
+			}
+			return res;
+		}
+		
+		static String[] readArrS(int N){
+			String[] res = new String[N];
+			for (int i = 0; i < N; i++){
+				res[i] = in.next();
+			}
+			return res;
+		}
+		
+		static double[] readArrD(int N){
+			double[] res = new double[N];
+			for (int i = 0; i < N; i++){
+				res[i] = in.nextDouble();
+			}
+			return res;
+		}
+		
+		static long[] readArrL(int N){
+			long[] res = new long[N];
+			for (int i = 0; i < N; i++){
+				res[i] = in.nextLong();
+			}
+			return res;
+		}
+		
+		static char[] readArrC(int N){
+			char[] res = new char[N];
+			for (int i = 0; i < N; i++){
+				res[i] = in.next().charAt(0);
+			}
+			return res;
+		}
+
+         static int bisect_right(int[] A, int x) {
+            return bisect_right(A, x, 0, A.length);
+        }
+
+         static int bisect_right(int[] A, int x, int lo, int hi) {
+            int N = A.length;
+            if (N == 0) {
+                return 0;
+            }
+            if (x < A[lo]) {
+                return lo;
+            }
+            if (x > A[hi - 1]) {
+                return hi;
+            }
+            for (;;) {
+                if (lo + 1 == hi) {
+                    return lo + 1;
+                }
+                int mi = (hi + lo) / 2;
+                if (x < A[mi]) {
+                    hi = mi;
+                } else {
+                    lo = mi;
+                }
+            }
+        }
+
+        static int bisect_left(int[] A, int x) {
+            return bisect_left(A, x, 0, A.length);
+        }
+
+        static int bisect_left(int[] A, int x, int lo, int hi) {
+            int N = A.length;
+            if (N == 0) {
+                return 0;
+            }
+            if (x < A[lo]) {
+                return lo;
+            }
+            if (x > A[hi - 1]) {
+                return hi;
+            }
+            for (;;) {
+                if (lo + 1 == hi) {
+                    return x == A[lo] ? lo : (lo + 1);
+                }
+                int mi = (hi + lo) / 2;
+                if (x <= A[mi]) {
+                    hi = mi;
+                } else {
+                    lo = mi;
+                }
+            }
+        }
+    }
+}
---
+++ Contests/Division 3/Round 481/B.java
@@ -0,0 +1,209 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class B
+{
+	static int MOD = 1000000007;
+    static Scanner in = new Scanner(System.in);
+    static PrintWriter p = new PrintWriter(System.out, true);
+
+
+
+    static int solve(){
+		int n = in.nextInt();
+		String s = in.next();
+		int xCount = 0;
+		int res = 0;
+		for (int i = 0; i < n; i++){
+			char curr = s.charAt(i);
+			if (curr == 'x'){
+				xCount++;
+				if(xCount > 2){
+					res++;
+				}
+			}
+			else{
+				xCount = 0;
+			}
+		}
+		p.println(res);
+		p.close();
+        return 0;
+    }
+
+    public static void main(String[] args)
+    {
+        solve();
+    }
+	
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+ 
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { return Integer.parseInt(next()); }
+ 
+        long nextLong() { return Long.parseLong(next()); }
+ 
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static class F{
+
+
+         static void swap(int[] a, int i, int j){
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+
+        static void swap(String[] a, int i, int j){
+            String temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+
+        static void swap(long[] a, int i, int j){
+            long temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+
+        static void swap(double[] a, int i, int j){
+            double temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+		
+		static int[] readArrI(int N){
+			int[] res = new int[N];
+			for (int i = 0; i < N; i++){
+				res[i] = in.nextInt();
+			}
+			return res;
+		}
+		
+		static String[] readArrS(int N){
+			String[] res = new String[N];
+			for (int i = 0; i < N; i++){
+				res[i] = in.next();
+			}
+			return res;
+		}
+		
+		static double[] readArrD(int N){
+			double[] res = new double[N];
+			for (int i = 0; i < N; i++){
+				res[i] = in.nextDouble();
+			}
+			return res;
+		}
+		
+		static long[] readArrL(int N){
+			long[] res = new long[N];
+			for (int i = 0; i < N; i++){
+				res[i] = in.nextLong();
+			}
+			return res;
+		}
+		
+		static char[] readArrC(int N){
+			char[] res = new char[N];
+			for (int i = 0; i < N; i++){
+				res[i] = in.next().charAt(0);
+			}
+			return res;
+		}
+
+         static int bisect_right(int[] A, int x) {
+            return bisect_right(A, x, 0, A.length);
+        }
+
+         static int bisect_right(int[] A, int x, int lo, int hi) {
+            int N = A.length;
+            if (N == 0) {
+                return 0;
+            }
+            if (x < A[lo]) {
+                return lo;
+            }
+            if (x > A[hi - 1]) {
+                return hi;
+            }
+            for (;;) {
+                if (lo + 1 == hi) {
+                    return lo + 1;
+                }
+                int mi = (hi + lo) / 2;
+                if (x < A[mi]) {
+                    hi = mi;
+                } else {
+                    lo = mi;
+                }
+            }
+        }
+
+        static int bisect_left(int[] A, int x) {
+            return bisect_left(A, x, 0, A.length);
+        }
+
+        static int bisect_left(int[] A, int x, int lo, int hi) {
+            int N = A.length;
+            if (N == 0) {
+                return 0;
+            }
+            if (x < A[lo]) {
+                return lo;
+            }
+            if (x > A[hi - 1]) {
+                return hi;
+            }
+            for (;;) {
+                if (lo + 1 == hi) {
+                    return x == A[lo] ? lo : (lo + 1);
+                }
+                int mi = (hi + lo) / 2;
+                if (x <= A[mi]) {
+                    hi = mi;
+                } else {
+                    lo = mi;
+                }
+            }
+        }
+    }
+}
