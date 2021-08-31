b019885d27a67cc2f1c728f4c858a5aaaadec537
jsn665 <64424116+jsn665@users.noreply.github.com>
2021-08-31 14:09:38 -0400

    Upload `Edit Distance` tabulation code

---
+++ CSES Problem Set/Dynamic Programming/Tabulation/Edit Distance.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve()
+{
+    string s, t;
+    cin >> s >> t;
+    int n = s.length();
+    int m = t.length();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+    for (int i = 0; i <= n; i++)
+    {
+        dp[i][0] = i;
+    }
+    for (int i = 0; i <= m; i++)
+    {
+        dp[0][i] = i;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s[i - 1] == t[j - 1])
+            {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = min({dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]}) + 1;
+            }
+        }
+    }
+    cout << dp[n][m] << "\n";
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
