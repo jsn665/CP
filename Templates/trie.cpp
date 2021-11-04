eae42aea55566f99ffbc0af694f70ac26533ddbb
jsn665 <64424116+jsn665@users.noreply.github.com>
2021-11-04 16:05:13 -0400

    Update trie.cpp

--- Templates/trie.cpp
+++ Templates/trie.cpp
@@ -28,4 +28,49 @@ class Node{
 
 };
 
+class Trie {
+public:
+    Node *root;
+    Trie() {
+        root = new Node();
+    }
 
+    void insert(string word) {
+	int N = word.length();
+	Node *dummy = root;
+	for(int i = 0; i < N; i++){
+		char key = word[i];
+		if(!dummy->containsKey(key)){
+			dummy->addKey(key);
+		}
+		dummy = dummy->getNode(key);
+	}
+	dummy->isComplete = true;
+    }
+
+    bool search(string word) {
+	int N = word.length();
+	Node *dummy = root;
+	for(int i = 0; i < N; i++){
+		char key = word[i];
+		if(!dummy->containsKey(key)){
+			return false;
+		}
+		dummy = dummy->getNode(key);
+	}
+	return dummy->isComplete;
+    }
+
+    bool startsWith(string prefix) {
+	int N = prefix.length();
+	Node *dummy = root;
+	for(int i = 0; i < N; i++){
+		char key = prefix[i];
+		if(!dummy->containsKey(key)){
+			return false;
+		}
+		dummy = dummy->getNode(key);
+	}
+	return true;
+    }
+};
