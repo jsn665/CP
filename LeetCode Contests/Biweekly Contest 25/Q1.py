f2a079c048a2375c768570a7f096f6eee71ddccb
jsn665 <64424116+jsn665@users.noreply.github.com>
2022-04-20 18:07:58 -0400

    Delete LeetCode Contests directory

--- LeetCode Contests/Biweekly Contest 25/Q1.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        larg = max(candies)
-        res = [False] * len(candies)
-        for i in range(len(candies)):
-            res[i] = True if candies[i] + extraCandies >= larg else False
-        return res
