218d2fb6e60717d3bc857b83609b7f09122284a7
jsn665 <64424116+jsn665@users.noreply.github.com>
2022-04-25 16:41:10 -0400

    Update circle_overlap.py

--- Templates/circle_overlap.py
+++ Templates/circle_overlap.py
@@ -5,3 +5,8 @@ def overlap(x1, y1, r1, x2, y2, r2):
     dist = sq(x1 - x2) + sq(y1 - y2)
     return dist < sq(r1 + r2)
 
+# dist < sq(r1 + r2) -> overlap
+# dist == sq(r1 + r2) -> touches
+# dist > sq(r1 + r2) -> no overlap or touch
+# dist <= sq(rx) point lies on the circle with radius rx
+
