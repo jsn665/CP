e9b887e1770c88d494063a1845546c896f5ef097
jsn665 <64424116+jsn665@users.noreply.github.com>
2022-05-04 13:29:55 -0400

    Update sparse_table.py

--- Templates/sparse_table.py
+++ Templates/sparse_table.py
@@ -18,18 +18,19 @@ class SparseTable:
         K = self.logs[N]
         self.st = [[0 for _ in range(N)] for __ in range(K + 1)]
         for i in range(N):
-            self.st[0][i] = A[i]
+            self.st[0][i] = (A[i], i)
 
         for i in range(1, K + 1):
             j = 0
             while j + (1 << i) - 1 < N:
-                self.st[i][j] = self.method(self.st[i - 1][j], self.st[i - 1][j + (1 << (i - 1))])
+                self.st[i][j] = self.method(self.st[i - 1][j], self.st[i - 1][j + (1 << (i - 1))], key= lambda x : x[0])
                 j += 1
 
     '''
     :param L: left bound of the subarray to be queried
     :param R: right bound of the subarray to be queried (inclusive)
+    :returns: tuple with (method result, index)
     '''
     def query(self, L, R):
         K = self.logs[R - L + 1]
-        return self.method(self.st[K][L], self.st[K][R - (1 << K) + 1])
+        return self.method(self.st[K][L], self.st[K][R - (1 << K) + 1], key= lambda x : x[0])
