f0c940d64bd73232bfa9671f006f685c5b9c6fb1
jsn665 <64424116+jsn665@users.noreply.github.com>
2023-02-18 15:59:14 -0500

    Update segment_tree_sum.py

--- Templates/segment_tree_sum.py
+++ Templates/segment_tree_sum.py
@@ -1,56 +1,66 @@
-# Segment Tree Sum Queries
-'''
-@:param: n -> size of input
-@:param: arr -> array to build
-'''
 class SegmentTree:
-    def __init__(self, n, arr):
-        self.N = n
-        self.tree = [0] * (4 * self.N)
-        def build(root, rootL, rootR):
-            if rootL == rootR:
-                self.tree[root] = arr[rootL]
-                return
-            left_child = 2 * root + 1
-            right_child = 2 * root + 2
-            mid = (rootL + rootR) // 2
-            build(left_child, rootL, mid)
-            build(right_child, mid + 1, rootR)
-            self.tree[root] = self.tree[left_child] + self.tree[right_child]
-        if len(arr):
-            build(0, 0, self.N - 1)
-
-
-    # query range [L,R] **R is inclusive**
-    def query(self, L, R):
-        return self._query_helper(L, R, 0, 0, self.N - 1)
-
-    def _query_helper(self, L, R, root, rootL, rootR):
-        if rootL >= L and rootR <= R:
-            return self.tree[root]
-        elif rootL > R or rootR < L:
-            return 0
-        left_child = 2 * root + 1
-        right_child = 2 * root + 2
-        mid = (rootL + rootR) // 2
-        return self._query_helper(L, R, left_child, rootL, mid) + self._query_helper(L, R, right_child, mid + 1, rootR)
-
-    # point update
-    def update(self, index, value):
-        return self._update_helper(index, 0, 0, self.N - 1, value)
-
-    def _update_helper(self, index, root, rootL, rootR, value):
-        if rootL == rootR:
-            self.tree[root] = value
-            return
-        left_child = 2 * root + 1
-        right_child = 2 * root + 2
-        mid = (rootL + rootR) // 2
-        if index >= rootL and index <= mid:
-            self._update_helper(index, left_child, rootL, mid, value)
+    def __init__(self, nums):
+        self.n = len(nums)
+        self.tree = [0] * (4 * self.n)
+        self.lazy = [0] * (4 * self.n)
+        self._build_tree(nums, 0, 0, self.n-1)
+    
+    def _build_tree(self, nums, idx, left, right):
+        if left == right:
+            self.tree[idx] = nums[left]
         else:
-            self._update_helper(index, right_child, mid + 1, rootR, value)
-        self.tree[root] = self.tree[left_child] + self.tree[right_child]
+            mid = (left + right) // 2
+            self._build_tree(nums, 2*idx+1, left, mid)
+            self._build_tree(nums, 2*idx+2, mid+1, right)
+            self.tree[idx] = self.tree[2*idx+1] + self.tree[2*idx+2]
+    
+    def _update(self, idx, left, right, ul, ur, val):
+        if self.lazy[idx] != 0:
+            self.tree[idx] += (right - left + 1) * self.lazy[idx]
+            if left != right:
+                self.lazy[2*idx+1] += self.lazy[idx]
+                self.lazy[2*idx+2] += self.lazy[idx]
+            self.lazy[idx] = 0
+        
+        if left > ur or right < ul:
+            return
+        
+        if left >= ul and right <= ur:
+            self.tree[idx] += (right - left + 1) * val
+            if left != right:
+                self.lazy[2*idx+1] += val
+                self.lazy[2*idx+2] += val
+            return
+        
+        mid = (left + right) // 2
+        self._update(2*idx+1, left, mid, ul, ur, val)
+        self._update(2*idx+2, mid+1, right, ul, ur, val)
+        self.tree[idx] = self.tree[2*idx+1] + self.tree[2*idx+2]
+    
+    def update(self, left, right, val):
+        self._update(0, 0, self.n-1, left, right, val)
+    
+    def _query(self, idx, left, right, ql, qr):
+        if self.lazy[idx] != 0:
+            self.tree[idx] += (right - left + 1) * self.lazy[idx]
+            if left != right:
+                self.lazy[2*idx+1] += self.lazy[idx]
+                self.lazy[2*idx+2] += self.lazy[idx]
+            self.lazy[idx] = 0
+        
+        if left > qr or right < ql:
+            return 0
+        
+        if left >= ql and right <= qr:
+            return self.tree[idx]
+        
+        mid = (left + right) // 2
+        left_sum = self._query(2*idx+1, left, mid, ql, qr)
+        right_sum = self._query(2*idx+2, mid+1, right, ql, qr)
+        return left_sum + right_sum
+    
+    def query(self, left, right):
+        return self._query(0, 0, self.n-1, left, right)
 
     def __str__(self):
         return str(self.tree)
